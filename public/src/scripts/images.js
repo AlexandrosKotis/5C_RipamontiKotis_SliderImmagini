2fdd7af4c6beec2dfcabd903f64063c24fd858c1
Angelo <ripamontiangelo@itis-molinari.eu>
2025-02-11 09:24:29 +0100

    images

--- public/src/scripts/images.js
+++ public/src/scripts/images.js
@@ -2,33 +2,41 @@ export function images(parentElement, pubSub) {
     let imgs = [];
 
     return {
+        build: function () {
+            pubSub.subscribe("images/add",  async (data) => {
+                await this.send(data);
+                await this.load();
+            });
+        },
         render: function () {
-            `<div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>`
+            let html = `<div id="carouselImagesIndicators" class="carousel slide">
+                <div class="carousel-indicators">
+                    ${
+                        imgs.map((img, index) => {
+                            return `<button type="button" data-bs-target="#carouselImagesIndicators" data-bs-slide-to="${index}" aria-current="${index === 0 ? "true" : "false"}" aria-label="Slide ${index}"></button>`
+                        }).join("")
+                    }
+                </div>
+                <div class="carousel-inner">
+                    ${
+                        imgs.map((img, index) => {
+                            return `<div class="carousel-item active">
+                                        <img src="${img.url}" class="d-block w-100" alt="Immagine n° ${img.id}">
+                                    </div>`
+                        }).join("")
+                    }
+                </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselImagesIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselImagesIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>`;
+            
+            parentElement.innerHTML = html;
         },
 
         send: async function (img) {
