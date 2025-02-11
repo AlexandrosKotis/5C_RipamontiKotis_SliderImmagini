b2cbef8dd9abc7da45bee54f5306182925be7b83
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-02-11 14:32:56 +0100

    modifided admin form

--- public/src/scripts/admin/adminForm.js
+++ public/src/scripts/admin/adminForm.js
@@ -1,47 +1,36 @@
 export function adminForm(parentElement, pubSub) {
-    let data = [];
-    let callback = null;
-    let closeRender = null;
-    
+
     return {
-        onsubmit: (callbackInput) => { callback = callbackInput },
-        closeRender: (callback) => closeRender = callback,
-        send: (string, value) => {
+        throw: function (string) {
             const element = document.getElementById("feed-div");
-            const cls = value === true ? "feedback-div" : "error-div";
-            for (css of element.classList) {
-                element.classList.remove(css);
-            }
-            element.classList.add(cls);
             element.innerText = string;
         },
-        render: () => {
-            parentElement.innerHTML =
-            data.map(()=>{
+        render: function () {
+            parentElement.innerHTML = `
+                <div class="flex flex-col items-center gap-4">
+                    <input id="file" name="file" type="file" accept="image/*" class="form-control required">
+                    <button id="button" type="button" class="btn btn-primary w-100">Upload</button>
+                </div>`
+                + "<div id='feed-div' class=''></div>"
+                + "<button type='button' class='btn btn-primary w-100' id='submitAdminFrom'>Sumbit</button>"
+                + "<button type='button' class='btn btn-warning w-100 cl' id='cancel'>Cancel</button>";
 
-                return `<div class="flex flex-col items-center gap-4">
-            <input id="file" name="file" type="file" accept="image/*" class="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 required">
-            <button id="button" type="button" class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition">Upload</button>
-        </div>`;
-        
-            }).join('\n')
-            + "<div id='feed-div'></div>"
-            + "<button type='button' class='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2' id='submit'>INVIA</button>"
-            + "<button type='button' class='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cl' id='cancel'>ANNULLA</button>";
-            ;
 
-            document.querySelector("#submit").onclick = () => {
-                const result = data.map((name) => {
-                    return document.querySelector("#" + name[0]).value;
-                });
-                callback(result);
+            document.querySelector("#submitAdminForm").onclick = () => {
+                const inputFile = document.querySelector('#file');
+                const formData = new FormData();
+                formData.append("file", inputFile.files[0]);
+                try {
+                    pubSub.publish("images/add", formData);
+                } catch (e) {
+                    console.log(e);
+                    this.throw(e.message);
+                }
             }
             document.querySelectorAll(".cl").forEach((e) =>
                 e.onclick = () => {
-                    closeRender();
+                    //implemet close modal
                 });
-
-                pubSub.publish("formSubmitted");
         },
     };
 };
