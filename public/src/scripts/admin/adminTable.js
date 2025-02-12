f54847814df116e2340bca6e8275c54a54fdadc8
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-02-12 13:46:08 +0100

    create tableAdmin

--- public/src/scripts/admin/adminTable.js
+++ public/src/scripts/admin/adminTable.js
@@ -1,16 +1,50 @@
-export function adminTable(parentElement, pubSub) {
+export const adminTable = (parentElement, pubSub) => {
+    let imagesData = [];
 
-    
     return {
-        render: function(){
-        
+        render: function () {
+            parentElement.innerHTML = `
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Anteprima</th>
+                            <th>Nome</th>
+                            <th>Azioni</th>
+                        </tr>
+                    </thead>
+                    <tbody id="image-list"></tbody>
+                </table>
+            `;
+
+            pubSub.subscribe("images/update", (images) => this.build(images));
         },
 
-        build: function(){
+        build: function (images) {
+            imagesData = images;
 
-        },
+            document.getElementById("image-list").innerHTML = images.map((image, index) => `
+                <tr class="${index % 2 === 0 ? "table-light" : "table-secondary"}">
+                    <td><img src="`+image.url+` alt="`+image.name+`" class="img-thumbnail" style="width: 80px;"></td>
+                    <td>`+image.name+`</td>
+                    <td>
+                        <button type="button" data-id="`+image.id+`" class="btn btn-warning edit-btn mx-1">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                        <button type="button" data-id="`+image.id+` "class="btn btn-danger remove-btn mx-1">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            `).join("");
 
+            // Gestione eventi per edit e remove
+            document.querySelectorAll(".edit-btn").forEach(button =>
+                button.onclick = () => pubSub.publish("images/update", button.dataset.id)
+            );
 
-    }
-
-}
+            document.querySelectorAll(".remove-btn").forEach(button =>
+                button.onclick = () => pubSub.publish("images/delte/:id", button.dataset.id)
+            );
+        }
+    };
+};
