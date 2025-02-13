922fa4df0ab0ecbc205df4c0435ec868d2e8c33c
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-02-13 11:53:55 +0100

    admin issue

--- public/src/index.js
+++ public/src/index.js
@@ -1,12 +1,18 @@
 // Imports
 import { createNavigator } from "./scripts/navigator.js";
 import { images } from "./scripts/images.js";
+import { adminForm } from "./scripts/admin/adminForm.js";
+import { adminTable } from "./scripts/admin/adminTable.js";
+import { credential } from "./scripts/admin/login.js";
 import { createPubSub } from "./scripts/pubSub.js";
 
 // Declarations & Initialization
 const navigator = createNavigator(document.getElementById("body"));
 const pubSub = createPubSub();
 const homePage = images(document.getElementById("carouselContentImages"), pubSub);
+const componentAdminForm = adminForm();
+const componentAdminTable = adminTable();
+const componentCredential = credential(); 
 
 // Go to home
 location.href = "#home"
