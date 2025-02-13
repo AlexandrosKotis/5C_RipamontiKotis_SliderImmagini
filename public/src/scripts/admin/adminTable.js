d41916da538b079efabeb3d20e79f07a658a0acd
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-02-13 10:37:23 +0100

    fixing the adminTable

--- public/src/scripts/admin/adminTable.js
+++ public/src/scripts/admin/adminTable.js
@@ -1,9 +1,13 @@
 export const adminTable = (parentElement, pubSub) => {
     let imagesData = [];
+    pubSub.subscribe("images/add", (data) => {
+        imagesData = data;
+        this.render();
+    })
 
     return {
         render: function () {
-            parentElement.innerHTML = `
+            let html = `
                 <table class="table table-striped">
                     <thead>
                         <tr>
@@ -12,39 +16,37 @@ export const adminTable = (parentElement, pubSub) => {
                             <th>Azioni</th>
                         </tr>
                     </thead>
-                    <tbody id="image-list"></tbody>
-                </table>
+                    <tbody id="image-list">
             `;
-
-            pubSub.subscribe("images/update", (images) => this.build(images));
-        },
-
-        build: function (images) {
-            imagesData = images;
-
-            document.getElementById("image-list").innerHTML = images.map((image, index) => `
+            html = images.map((image, index) => `
                 <tr class="${index % 2 === 0 ? "table-light" : "table-secondary"}">
                     <td><img src="`+image.url+` alt="`+image.name+`" class="img-thumbnail" style="width: 80px;"></td>
                     <td>`+image.name+`</td>
                     <td>
+                    <!--
                         <button type="button" data-id="`+image.id+`" class="btn btn-warning edit-btn mx-1">
                             <i class="fas fa-edit"></i>
                         </button>
+                    -->
                         <button type="button" data-id="`+image.id+` "class="btn btn-danger remove-btn mx-1">
                             <i class="fas fa-trash"></i>
                         </button>
                     </td>
                 </tr>
             `).join("");
+            html="</tbody></table>"
 
             // Gestione eventi per edit e remove
-            document.querySelectorAll(".edit-btn").forEach(button =>
+            /*
+            document.querySelectorAll(".edit-btn").forEach((button, index) =>
                 button.onclick = () => pubSub.publish("images/update", button.dataset.id)
-            );
+            );*/
 
             document.querySelectorAll(".remove-btn").forEach(button =>
                 button.onclick = () => pubSub.publish("images/delte/:id", button.dataset.id)
             );
-        }
+
+            parentElement.innerHTML = html;
+        },
     };
 };
